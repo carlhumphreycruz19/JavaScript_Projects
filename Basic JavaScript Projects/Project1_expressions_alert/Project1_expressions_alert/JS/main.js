10d9a1412d2c7b8e1546322d887f0dc3d057b3ae
carlhumphreycruz19 <carlhumphrey_cruz@yahoo.ca>
2025-06-20 16:51:47 -0400

    basic Javascript project
    using basic javascripts

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,41 @@
+<!--indexedDB.html-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Basic JavaScript Projects Assignment</title></title>
+    <meta charset="UTF-8">
+    <script src="JS/main.js"></script>
+</head>
+<body>
+</body>
+</html>
+
+//main.js
+
+//window.alert method
+window.alert("Hello, World!");
+//document.write method
+document.write("Hello, World!");
+//creating a variable
+var sent1="This is a string";
+var sent2="This is another string";
+//displaying the variable
+document.write(sent1 + sent2);
+a = "Hello"
+print(a)
+window.alert("a");
+document.write("Lisa told Bart, \"knock it off, Bart! or i'll tell dad!\"<br>\"eat my shorts!\"bart responded.");
+document.write("\"Be who you are and say what you feel,"
++" because those who mind don't matter and those who matter don't mind.\""
++"<br>Dr. Seuss");
+var Family ="The Arezzinis" , Dad = "Jeremiah", Mom = "Hermoine",
+daughter = " Penny", son = "Zorro";
+document.write(Dad);
+//an expression
+document.write(3 + 3);
+//defining a function
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
