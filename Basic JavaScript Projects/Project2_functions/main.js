631450df70ac141beb865da08a8017603f3a7681
carlhumphreycruz19 <carlhumphrey_cruz@yahoo.ca>
2025-06-20 23:10:14 -0400

    i am learning JavaScript

---
+++ Basic JavaScript Projects/Project2_functions/main.js
@@ -0,0 +1,14 @@
+//main.js
+
+//creating a variable
+var sent1="This is a string";
+var sent2="This is another string";
+//displaying the variable
+document.write(sent1 + sent2);
+a = "Hello"
+print(a)
+ function myFunction () {
+        var sentence =" I am Learning";
+        sentence += " a lot from this book!";
+        document.getElementById("Concatenate").innerHTML = sentence;
+
